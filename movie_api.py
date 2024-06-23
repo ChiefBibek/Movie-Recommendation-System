9bb14c7a1dd4972fa56a4fe7e5f91f9afa8c1f57
Bibek Bikram Shahi <114321426+ChiefBibek@users.noreply.github.com>
2024-06-23 15:50:14 +0545

    Update movie_api.py

--- movie_api.py
+++ movie_api.py
@@ -24,4 +24,4 @@ def recommend_movies():
     return jsonify(recommendations)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=10000)
