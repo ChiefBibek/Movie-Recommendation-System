77b6b267e311631c80cf36ee385a2272efa71fea
AnwitDahal <dahalanwit0707@gmail.com>
2024-07-01 22:46:49 +0545

    Loader changed to DNA

--- Frontend And Backend/app/src/Components/Loader.jsx
+++ Frontend And Backend/app/src/Components/Loader.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { FidgetSpinner } from "react-loader-spinner";
+import { DNA } from "react-loader-spinner";
 
 const Loader = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen ">
         <img src="/Logo.png" alt="" className="w-60"/>
-      <FidgetSpinner
+      <DNA
         visible={true}
         height="80"
         width="80"
-        ariaLabel="fidget-spinner-loading"
+        ariaLabel="dna-loading"
         wrapperStyle={{}}
         wrapperClass="fidget-spinner-wrapper"
-        backgroundColor="#BB86FC"
-        ballColors={["red", "green", "yellow"]}
+        // backgroundColor="#BB86FC"
+        // ballColors={["red", "green", "yellow"]}
 
       />
     </div>
