add42ef6c21ea2aa012342afcfb01c5f9d742a3b
ChiefBibek <114321426+ChiefBibek@users.noreply.github.com>
2024-07-06 21:32:39 +0545

    model improvement

--- movie_api.py
+++ movie_api.py
@@ -37,4 +37,4 @@ def recommend_movies():
     return jsonify(recommendations)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=10000)
+    app.run(debug=False, host='0.0.0.0', port=10000)
