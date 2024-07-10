33d51796bcffa40c6f30ce0a6797bf3d05cb9b00
AnwitDahal <dahalanwit0707@gmail.com>
2024-07-11 00:26:22 +0545

    Hello World

--- Frontend And Backend/app/src/Components/MainBody.jsx
+++ Frontend And Backend/app/src/Components/MainBody.jsx
@@ -13,7 +13,7 @@ const MainBody = ({ movieres }) => {
   }, [movieres]);
 
   return (
-    <div className="my-7 px-1 md:px-20">
+    <div className="my-7 ">
       {showPopup && (
         <Popup
           message={`No actors matched with '${movieres}'`}
@@ -22,31 +22,33 @@ const MainBody = ({ movieres }) => {
       )}
       {!showPopup && Array.isArray(movieres) && (
         <>
-        <hr />
-          <h1 className="text-[#BB86FC] font-semibold sm:text-lg md:text-xl lg:text-2xl font-list_title text-center mb-5 md:text-start">
-            Top 10 Recommended Movies are:
-          </h1>
-          <ul className="flex flex-wrap gap-8 justify-center md:justify-start">
-            {movieres.map(({ poster, rating, title }, index) => (
-              <li key={index} className="text-[#F5F1ED] font-list">
-                <div className="w-[10rem] sm:w-[12rem] md:w-[14rem] lg:w-[15rem] bg-zinc-800 h-full">
-                  <img
-                    src={`${poster}`}
-                    alt={`${title}`}
-                    className="w-full h-auto"
-                  />
-                  <div className="flex flex-col text-[0.7rem] p-3">
-                    <h2 className="uppercase font-semibold text-base text-[#FFF9FB] sm:text-lg">
-                      {title}
-                    </h2>
-                    <h2 className="text-sm text-[#D3D4D9] sm:text-base">
-                      IMDb: {rating}
-                    </h2>
+          <hr />
+          <div className="mt-2 px-1 md:px-20">
+            <h1 className="text-[#BB86FC] font-semibold sm:text-lg md:text-xl lg:text-2xl font-list_title text-center mb-5 md:text-start">
+              Top 10 Recommended Movies are:
+            </h1>
+            <ul className="flex flex-wrap gap-8 justify-center md:justify-start">
+              {movieres.map(({ poster, rating, title }, index) => (
+                <li key={index} className="text-[#F5F1ED] font-list">
+                  <div className="w-[10rem] sm:w-[12rem] md:w-[14rem] lg:w-[15rem] bg-zinc-800 h-full">
+                    <img
+                      src={`${poster}`}
+                      alt={`${title}`}
+                      className="w-full h-auto"
+                    />
+                    <div className="flex flex-col text-[0.7rem] p-3">
+                      <h2 className="uppercase font-semibold text-base text-[#FFF9FB] sm:text-lg">
+                        {title}
+                      </h2>
+                      <h2 className="text-sm text-[#D3D4D9] sm:text-base">
+                        IMDb: {rating}
+                      </h2>
+                    </div>
                   </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
         </>
       )}
     </div>
