f3066b9cd3586ec5fc18fac462dd335fedcd2d06
Anwit Dahal <dahalanwit0707@gmail.com>
2026-01-13 11:12:17 +0545

    Added dynamic date in footer

--- Frontend And Backend/app/src/Components/Footer.jsx
+++ Frontend And Backend/app/src/Components/Footer.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  return (
-    <footer className='bg-zinc-800 text-[#BB86FC] text-sm tracking-tighter flex justify-center items-center flex-col p-2 md:py-4 md:text-base'>
-        <div>
-         &copy; 2024 Movie Recommendation. All rights reserved.
-        </div>
-        <div>
-            Made by <a href="https://github.com/ChiefBibek" target='_blank' className='hover:underline'>Bibek</a> and <a href="https://github.com/AnwitDahal" target='_blank' className='hover:underline'>Anwit</a>
-        </div>
-    </footer>
-  )
-}
+  const currentYear = new Date().getFullYear();
 
-export default Footer
+  return (
+    <footer className="bg-zinc-800 text-[#BB86FC] text-sm tracking-tighter flex justify-center items-center flex-col p-2 md:py-4 md:text-base">
+      <div>&copy; {currentYear} Movie Recommendation. All rights reserved.</div>
+      <div>
+        Made by{" "}
+        <a
+          href="https://github.com/ChiefBibek"
+          target="_blank"
+          className="hover:underline"
+        >
+          Bibek
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://github.com/AnwitDahal"
+          target="_blank"
+          className="hover:underline"
+        >
+          Anwit
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
